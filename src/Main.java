b47d0bb42f2932a34c5eb08b9d2c15bd6594f178
NicolasCastonguay <nicolas.castonguay.3@ulaval.ca>
2024-03-04 21:27:54 -0500

    vf

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,17 @@
 public class Main {
     public static void main(String[] args) {
         SixQueens game = new SixQueens(); // Initialisation du jeu
-
-        // Use A* search algorithm to find the solution
         a_star_search(game);
-
-        // Print the final board
         System.out.println("Final board:");
         game.printBoard();
     }
 
-    // A* search algorithm
+    // A*
     public static void a_star_search(SixQueens game) {
         while (!game.checkWin()) {
-            // Get the next move with the lowest heuristic value
             int[] nextMove = game.getNextMove();
             int row = nextMove[0];
             int col = nextMove[1];
-
-            // Add the queen to the next move
             game.addQueen(row, col);
         }
     }
--- src/SixQueens.java
+++ src/SixQueens.java
@@ -17,22 +17,22 @@ public class SixQueens {
     // Prédicat pour ajouter une reine
     public void addQueen(int row, int col) {
         // Corrigez la chaîne de requête Prolog
-        String queryString = String.format("add_queen(Board, %d, %d, NewBoard)", row + 1, col + 1);
+        String queryString = String.format("add_queen(Board, %d, %d, NewBoard)", row - 1, col - 1);
         Query.hasSolution(queryString);
         // Met à jour le plateau en Java
-        board[row][col] = 1;
+        board[row-1][col-1] = 1;
     }
 
     // Déplace une reine de la colonne C et de la ligne R vers une nouvelle ligne
     public void moveQueen(int oldRow, int oldCol, int newRow) {
         // Vérifie d'abord si la nouvelle position est sûre
-        if (isSafe(newRow, oldCol)) {
+        if (isSafe(newRow-1, oldCol-1)) {
             // Corrigez la chaîne de requête Prolog
-            String queryString = String.format("move_queen(Board, %d, %d, %d, NewBoard)", oldCol + 1, oldRow + 1, newRow + 1);
+            String queryString = String.format("move_queen(Board, %d, %d, %d, NewBoard)", oldCol - 1, oldRow - 1, newRow - 1);
             Query.hasSolution(queryString);
             // Met à jour le plateau en Java
-            board[oldRow][oldCol] = 0; // Supprime la reine de l'ancienne position
-            board[newRow][oldCol] = 1; // Place la reine dans la nouvelle position
+            board[oldRow-1][oldCol-1] = 0; // Supprime la reine de l'ancienne position
+            board[newRow-1][oldCol-1] = 1; // Place la reine dans la nouvelle position
         } else {
             System.out.println("Impossible de déplacer la reine à cette position.");
         }
@@ -41,16 +41,16 @@ public class SixQueens {
     // Prédicat pour supprimer une reine
     public void removeQueen(int row, int col) {
         // Corrigez la chaîne de requête Prolog
-        String queryString = String.format("remove_queen(Board, %d, %d, NewBoard)", row + 1, col + 1);
+        String queryString = String.format("remove_queen(Board, %d, %d, NewBoard)", row - 1, col - 1);
         Query.hasSolution(queryString);
         // Met à jour le plateau en Java
-        board[row][col] = 0;
+        board[row-1][col-1] = 0;
     }
 
     // Vérifie si une reine peut être placée aux coordonnées données
     public boolean isSafe(int row, int col) {
         // Corrigez la chaîne de requête Prolog
-        String queryString = String.format("valid_position(Board, (%d, %d))", col + 1, row + 1);
+        String queryString = String.format("valid_position(Board, (%d, %d))", col - 1, row - 1);
         return Query.hasSolution(queryString);
     }
 
@@ -81,63 +81,25 @@ public class SixQueens {
         return listBuilder.toString();
     }
 
-    // Convertit une liste Prolog en plateau
-    // Convertit une liste Prolog en plateau
-    public int[][] listToBoard(Term prologList) {
-        int[][] board = new int[SIZE][SIZE]; // Initialise un nouveau plateau
-        Term[] lists = prologList.toTermArray();
-        for (int i = 0; i < lists.length; i++) {
-            Term innerList = lists[i];
-            Term[] innerArray = innerList.toTermArray();
-            for (int j = 0; j < innerArray.length; j++) {
-                board[i][j] = innerArray[j].intValue();
-            }
-        }
-        return board;
-    }
-
-    // Get the next move with the lowest heuristic value
+    // Obtient le prochain coup avec la plus faible valeur heuristique
     public int[] getNextMove() {
         int[] nextMove = new int[2];
         int minHeuristic = Integer.MAX_VALUE;
-
-        // Iterate over all possible moves and calculate their heuristic value
         for (int row = 0; row < SIZE; row++) {
             for (int col = 0; col < SIZE; col++) {
                 if (isSafe(row, col)) {
-                    int heuristic = calculateHeuristic(row, col);
-                    if (heuristic < minHeuristic) {
-                        minHeuristic = heuristic;
-                        nextMove[0] = row;
-                        nextMove[1] = col;
+                    String prologList = boardToList(board);
+                    String query = String.format("heuristic(%s, [%d, %d], Heuristic)", prologList, row, col);
+                    Map<String, Term> solution = Query.oneSolution(query);
+                    if (solution != null) {
+                        int heuristic = solution.get("Heuristic").intValue();
+                        if (heuristic < minHeuristic) {
+                            minHeuristic = heuristic;
+                            nextMove[0] = row;
+                            nextMove[1] = col;
+                        }
                     }
                 }
             }
         }
         return nextMove;
-    }
-
-    // Calculate the heuristic value for the given move
-    private int calculateHeuristic(int row, int col) {
-        String prologList = boardToList(board); // Convert the board to a Prolog list
-        String query = String.format("heuristic(%s, [(%d, %d)], Heuristic)", prologList, row + 1, col + 1);
-        Query q = new Query(query);
-        if (q.hasSolution()) {
-            return q.oneSolution().get("Heuristic").intValue();
-        }
-        return Integer.MAX_VALUE; // Default to maximum value if no solution found
-    }
-
-    public void printBoard() {
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                if (board[i][j] == 1) {
-                    System.out.print("Q "); // Queen present at this position
-                } else {
-                    System.out.print("_ "); // Empty position
-                }
-            }
-            System.out.println(); // Move to the next row
-        }
-    }
-}
